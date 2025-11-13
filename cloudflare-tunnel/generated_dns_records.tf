fc8cae4126f5ad675198b7e8c36dc999fa99149b
Mircea Casapu <mircea.casapu@gmail.com>
2025-11-13 23:21:50 +0000

    feat: import existing resources in tf config

---
+++ cloudflare-tunnel/generated_dns_records.tf
@@ -0,0 +1,51 @@
+resource "cloudflare_dns_record" "terraform_managed_resource_27b84829b17fbafb6c98ad518331a208_0" {
+  comment  = "Domain verification record"
+  content  = "203.0.113.10"
+  name     = "www.home-server.me"
+  proxied  = true
+  tags     = []
+  ttl      = 1
+  type     = "A"
+  zone_id  = "97c1a5749e2e19a69ae4132f95f633cd"
+  settings = {}
+}
+
+resource "cloudflare_dns_record" "terraform_managed_resource_8f22cd35e6f3cc9e39ccd94b2a949363_1" {
+  content = "2a32c37d-447c-4d24-9256-9deb86bc686f.cfargotunnel.com"
+  name    = "ha.home-server.me"
+  proxied = true
+  tags    = []
+  ttl     = 1
+  type    = "CNAME"
+  zone_id = "97c1a5749e2e19a69ae4132f95f633cd"
+  settings = {
+    flatten_cname = false
+  }
+}
+
+resource "cloudflare_dns_record" "terraform_managed_resource_6b1094bfea3ccf9296fbd38eab9a6265_2" {
+  content = "fc1846ca-b4fa-43ba-bb54-13469ad1f9f1.cfargotunnel.com"
+  name    = "home-server.me"
+  proxied = true
+  tags    = []
+  ttl     = 1
+  type    = "CNAME"
+  zone_id = "97c1a5749e2e19a69ae4132f95f633cd"
+  settings = {
+    flatten_cname = false
+  }
+}
+
+resource "cloudflare_dns_record" "terraform_managed_resource_2ed691eb805ce7bcc49600fd0424f935_3" {
+  content = "2a32c37d-447c-4d24-9256-9deb86bc686f.cfargotunnel.com"
+  name    = "plex.home-server.me"
+  proxied = true
+  tags    = []
+  ttl     = 1
+  type    = "CNAME"
+  zone_id = "97c1a5749e2e19a69ae4132f95f633cd"
+  settings = {
+    flatten_cname = false
+  }
+}
+
---
+++ cloudflare-tunnel/generated_tunnels.tf
@@ -0,0 +1,30 @@
+resource "cloudflare_zero_trust_tunnel_cloudflared" "terraform_managed_resource_2a32c37d-447c-4d24-9256-9deb86bc686f_0" {
+  account_id = "ed6d4bb3828b44b941ccbbd0dc250af7"
+  config_src = "local"
+  name       = "homeserver"
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "terraform_managed_resource_ed6d4bb3828b44b941ccbbd0dc250af7_0" {
+  account_id = "ed6d4bb3828b44b941ccbbd0dc250af7"
+  source     = "local"
+  tunnel_id  = "2a32c37d-447c-4d24-9256-9deb86bc686f"
+  config = {
+    __configuration_flags = {
+      no-autoupdate = "true"
+    }
+    ingress = [{
+      hostname      = "plex.home-server.me"
+      originRequest = {}
+      service       = "http://localhost:32400"
+      }, {
+      hostname      = "ha.home-server.me"
+      originRequest = {}
+      service       = "http://localhost:8123"
+      }, {
+      originRequest = {}
+      service       = "http_status:404"
+    }]
+    warp-routing = {}
+  }
+}
+
